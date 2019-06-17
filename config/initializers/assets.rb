99f683f733b82687b8b8a4568dd590e582c2a787
HauMin <haumin1201@gmail.com>
2019-06-17 15:10:58 +0700

    Admin User

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w(chat.js chat.css)
+Rails.application.config.assets.precompile += %w(new.js common/*.js)
+Rails.application.config.assets.precompile += %w(edit.js common/*.js)
